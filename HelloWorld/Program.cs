6370746c7771c41502d9a500ca02fb781c36c6a2
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-01-22 10:39:30 -0500

    Commit Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;//Directive to include a namespace (library)
+
+namespace HelloWorld
+    //Comments begin with //
+    /*  Multiple lines of comments
+     *  fsdfsd
+     *  fdsfds
+     *  fdfsd
+     *  */
+{
+    class Program
+    {
+        static void Main(string[] args)//Entry point
+        {
+            Console.WriteLine("Hello World!!! I am a brand new project in C#");//Write = Display
+            
+            Console.WriteLine("This is the 2nd message \n I want to display to the Console");
+
+            Console.ReadKey();//Read = Input
+            
+
+        }
+    }
+}
