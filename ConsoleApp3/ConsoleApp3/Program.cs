70094558a108e5267760816e2ef7e09eb7b86360
Manjula Chandirasekaran <Manjula.Chandirasekaran@tri-c.edu>
2020-01-22 13:52:32 -0500

    Lab 2

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int origNum = 10;
+            int result = 0;
+
+            result = origNum + 3; //Add 3 
+            result = result * 2;
+            result = result - 4;
+            result = result / 2;
+            result = result - origNum;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
