b88b44b07f2c20b58211fb9183882b25e2f21261
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-01-25 13:45:36 -0500

    Week 2 Exercise 1

---
+++ Week2Exercise1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Week 2 Exercise 1");
+
+            //Data types
+            /* numbers and 
+             * texts*/
+
+            int integerNum;//example 5
+            double realNum;//example 5.43
+            string message;//example "hello"
+
+            //Assignment of values to variables
+            integerNum = 5;
+            realNum = 5.46;
+            message = "Hello";//enclose text in quotes
+
+            Console.WriteLine(integerNum);
+            Console.WriteLine(realNum);
+            Console.WriteLine(message);
+            Console.WriteLine("End of program");
+            Console.ReadKey();
+
+        }
+    }
+}
