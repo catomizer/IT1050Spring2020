2160ce0466b5d62d61a5e545a31cff9071d35886
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-02-08 08:26:56 -0500

    w3e3

---
+++ Week5Exercise3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = -1;
+
+            while (num >= 0)
+            {
+                num = num * 2;
+            }
+
+            Console.WriteLine("Sum after while loop is {0}",num);
+
+            do
+            {
+                num = num * 2;
+
+            } while (num >= 0);
+
+            Console.WriteLine("Sum after Do-while loop is {0}", num);
+
+
+            Console.ReadKey();
+        }
+    }
+}
