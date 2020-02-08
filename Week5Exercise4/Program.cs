f89a230dd264365fe83b5628638900ddf993b42a
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-02-08 08:37:01 -0500

    w5e4

---
+++ Week5Exercise4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* For Loop*/
+            Console.WriteLine("Week5Exercise4 - For loop");
+
+            double score = 0.0;
+            double sum = 0.0;
+            double avg = 0.0;
+
+            for (int counter=0;counter < 5 ; counter++)//counter++ => counter = counter + 1
+            {
+                string scoreAsText = Console.ReadLine();
+                score = Convert.ToDouble(scoreAsText);
+                //sum = sum + score;
+                sum += score;
+            }
+
+            avg = sum / 5;
+            Console.WriteLine("sum and average are {0} {1}", sum, avg);
+            Console.ReadKey();
+
+        }
+    }
+}
