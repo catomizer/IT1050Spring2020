446939818f46709dca963ad2936ff007fafadbdd
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-02-16 09:47:10 -0500

    Week 5 Sprint code checkin

---
+++ Week5Sprint1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Sprint1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Week 5 Spring Add 10 numbers
+            
+            int number = 0;
+            int counter = 0;
+            int sum = 0;
+
+            Console.WriteLine("Week 5 Sprint Add 10 numbers");            
+
+            while (counter < 10)
+            {
+                Console.WriteLine("Enter a number");
+                number = Int32.Parse(Console.ReadLine());
+                sum = sum + number;
+                counter = counter + 1;
+            }
+
+            Console.WriteLine("The sum of 10 given numbers is {0}", sum);
+            Console.ReadKey();
+
+           
+        }
+    }
+}
---
+++ Week5Sprint2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Sprint2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Week 5 Sprint Multiplication Table");
+            Console.WriteLine("Enter a number");
+            int multiplicant = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("The multiplication table for {0}:", multiplicant);
+            int product = 1;
+            for (int counter = 1; counter <= 10; counter++) //write multiplication table from 1 to 10
+            {
+                product = counter * multiplicant;
+                Console.WriteLine("{0} x {1} = {2}", counter, multiplicant, product);
+            }
+            Console.ReadKey();
+        }
+    }
+}
