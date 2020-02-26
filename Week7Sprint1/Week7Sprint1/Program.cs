7cacbbd683ffaf743b87808d65a3c7fbf9202c2a
Manjula Chandirasekaran <Manjula.Chandirasekaran@tri-c.edu>
2020-02-26 13:08:09 -0500

    Week 7

---
+++ Week7Sprint1/Week7Sprint1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7Sprint1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number 0-9");//Prompt user for input
+            string input = Console.ReadLine();//Read and save
+
+            switch (input)//Check value of input
+            {
+                case "0"://if input is equal to "0"
+                    Console.WriteLine("You entered 0");
+                    break;
+                case "1"://if input is equal to "1"
+                    Console.WriteLine("You entered 1");
+                    break;
+                default://if input is some other value not liste above
+                    Console.WriteLine("Your input is invalid");
+                    break;
+            }
+            Console.WriteLine("All done");
+            Console.ReadKey();
+        }
+    }
+}
