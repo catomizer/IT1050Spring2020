5878c16910a65bacbbfd806ac3c197fc2cca7e02
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-03-18 18:55:32 -0400

    Week 9 changes

--- Week9Sprint3/Week9Sprint3/Program.cs
+++ Week9Sprint3/Week9Sprint3/Program.cs
@@ -11,26 +11,37 @@ namespace Week9Sprint3
         static void Main(string[] args)
         {
             Console.WriteLine("Week 9 Sprint 3 - Assign values to an array");
-            //Goal - Get input of 5 employee salaries and add to the salaryArray
+            
+            //Goal - Get input of 5 employee salaries and add to the salaryArray. Calculate tax @ 18%
+            double[] salaryArray = new double[5];//declaring an empty array
+            double[] taxArray = new double[5];
 
-            double salary = 0.0;          
-            double taxPercent = 0.18;
-
-            //Create 2 empty arrays to hold 5 salaries and taxes
-            double[] salaryArray = new double[5];
-            double[] tax = new double[5];
-
-            //For 5 employees, get salary as input and add to salaryArray
-            for (int i = 0; i < 5; i++)
+            for(int i=0; i < 5; i++)
             {
-                Console.WriteLine("Please enter the salary of Employee # {0}", i);
-                salary = Convert.ToDouble(Console.ReadLine());
-                salaryArray[i] = salary;//assigning a value 
+                //Get input and save to a variable
+                Console.WriteLine("Enter the salary of Employee # {0}", i);
+                double salary = Convert.ToDouble(Console.ReadLine());
 
-                tax[i] = salary * taxPercent;
-                Console.WriteLine("Salary {0} Tax {1}", salaryArray[i], tax[i]);                
+                salaryArray[i]=salary;
+                taxArray[i] = salary * 0.18;
+
+                //Console.WriteLine("Salary = {0} and Tax = {1}", salaryArray[i], taxArray[i]);
+
             }
 
+            Console.WriteLine("Salary array");
+            Console.WriteLine("-------------");
+            foreach(double salary in salaryArray)
+            {
+              Console.WriteLine(salary);
+            }
+
+            Console.WriteLine("Tax array");
+            Console.WriteLine("-------------");
+            foreach(double tax in taxArray)
+            {
+              Console.WriteLine(tax);
+            }
 
             Console.WriteLine("All done");
             Console.ReadKey();
