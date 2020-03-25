b80e4fc23ba92453a164c65f5fb3ac7acd41162d
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-03-25 09:46:44 -0400

    Commit week 10

---
+++ Week10Sprint1/Cat.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10Sprint1
+{
+    class Cat
+    {
+        string name;
+        string color;
+        string ID;
+        int age;
+
+        bool hungry;
+        bool sleepy;
+
+        //Default constructor
+        public Cat()
+        {
+            age = 0;
+            name = "No name yet";
+            color = "none";
+            ID = "Not assigned";
+
+            hungry = false;
+            sleepy = false;
+        }
+
+        //Constructor with parameters
+        public Cat(string cName, string cColor, int cAge, string cID)
+        {
+            name = cName;
+            age = cAge;
+            color = cColor;
+            ID = cID;
+
+            hungry = false;
+            sleepy = false;
+        }
+
+        //method #1
+        public void intro()
+        {
+            Console.WriteLine("Hi My name is {0}. My age is {1}. My fur color is {2} and my ID is {3}", name, age,color, ID);
+        }
+
+        public void play()
+        {
+            Console.WriteLine("I love to play. Come join me!");
+        }
+
+        public void eat()
+        {
+            Console.WriteLine("Thanks for feeding me!");
+        }
+
+        public void sleep()
+        {
+            Console.WriteLine("I am tired! Zzzzz");
+        }
+    }
+}
---
+++ Week10Sprint1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10Sprint1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I have 3 cats
+            /* cat 1 is called fluff, is 4 years old, id="A322" and color is orange
+             * cat 2 is called "snowbell", is 1 year old, id="R333" and color is white
+             * cat 3 is called "stripey", is 6 years old, id="S123" and color is black
+             * */
+
+            /*string[] catNames = new string[] { "fluff", "snowbell","stripey"};
+            string[] catIDs = new string[] { "A322", "R333", "S123"};
+            int[] catAges = new int[] { 4,1,6 };
+            string[] catColors = new string[] {"orange", "white","black" };
+
+            Console.WriteLine("Enter cat name");
+            catNames[3] = Console.ReadLine();
+
+            Console.WriteLine("Enter cat id");
+            catIDs[3] = Console.ReadLine();
+
+            Console.WriteLine("Enter cat age");
+            catAges[3] = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter cat color");
+            catColors[3] = Console.ReadLine(); */
+
+            Cat myCat = new Cat();//Create an object
+            myCat.intro();
+
+            Cat fluff = new Cat("fluff", "orange", 4, "A332");//Object name is fluff
+            fluff.intro();
+            fluff.play();
+           
+
+            Cat myCat2 = new Cat("snowbell", "white", 1, "R333");//Object name is myCat2
+            myCat2.intro();
+            myCat2.sleep();
+
+            Cat myCat3 = new Cat("stripey", "black", 6, "S123");//Object name is fluff
+            myCat3.intro();
+            myCat3.eat();
+
+            Console.ReadKey();
+
+        }
+    }
+}
