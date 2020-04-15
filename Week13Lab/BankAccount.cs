9c02579db4147efdef12377837af563f25df439e
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-04-15 16:48:57 -0400

    Week13Lab

---
+++ Week13Lab/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13Lab
+{
+    class BankAccount
+    {
+        public string AcctNbr { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public BankAccount()
+        {
+            Balance = 1000;
+        }
+
+        public void Deposit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdraw(double amount)
+        {
+            throw new NotImplementedException();
+        }
+        public void CalculateInterest() { }
+
+    }
+}
---
+++ Week13Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write code to implement the test cases below
+
+            /*Test case 1
+             * Create a savingsaccount object
+             * Deposit $2000
+             * Withdraw $5000
+             * Calculate interest
+             * Withdraw $500
+            */
+
+            /*Test case 2
+            * Create a checkingaccount object
+            * Deposit $2000
+            * Withdraw $5000
+            * Calculate interest
+            * Withdraw $500
+           */
+
+        }
+    }
+}
